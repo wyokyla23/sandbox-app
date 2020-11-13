27e6b3c44aed651142d50e3ea0eca37d7ade0438
wyokyla23 <wyokyla23@gmail.com>
2020-11-13 16:09:01 -0600

    feat: created todo list with some styling

---
+++ src/components/Todo.js
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import "./todo-styles.css";
+import { customData } from "./data";
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Input from '@material-ui/core/Input'
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  todoWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxWidth: '1200px',
+    margin: '0 auto',
+  },
+  columnWrapper: {
+    width: '100%',
+    minHeight: '600px',
+    height: '100%',
+    backgroundColor: '#232323;',
+    display: 'flex',
+    color: '#aaaaaa;',
+    fontFamily: 'Londrina Shadow, cursive',
+    fontSize: '2.5rem',
+  },
+  finished: {
+    flex: '1',
+    textAlign: 'center',
+  },
+  listItems: {
+    width: '90%',
+    listStyleType: 'none',
+  },
+  clearButton: {
+    paddingInlineStart: '15px',
+    marginTop: '20px',
+    marginBottom: '10px'
+  },
+  todoHeader: {
+    width: '70%',
+    display: 'flex',
+    justifyContent: 'space-around',
+    textAlign: 'center',
+    marginBottom: '30px',
+    fontSize: '1.2rem',
+    lineHeight: '1px;',
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '100%'
+    }
+  },
+  input: {
+    width: '50%',
+    borderRadius: '20px',
+    [theme.breakpoints.down('md')]: {
+      width: '60%',
+    },
+  },
+  button: {
+    color: '#232323',
+    backgroundColor: '#aaaaaa'
+  }
+}))
+
+
+export default function TodoList() {
+  const [todos, setTodos] = useState(customData)
+  const [finished, setFinished] = useState([])
+  const classes = useStyles()
+  console.log(todos, finished)
+  const completeItem = (item) => (event) => {
+    setTodos(todos.filter((innerItem) => {
+      return item.id !== innerItem.id
+    }))
+    setFinished(finished.concat(item))
+  }
+
+  const deleteItem = (item) => (event) => {
+    setFinished(finished.filter((innerItem) => {
+      return item.id !== innerItem.id
+    }))
+  }
+
+  const clearItems = () => (event) => {
+    setTodos([])
+  }
+
+  const handleChange = () => (event) => {
+    console.log(event.target.value)
+  }
+
+  return (
+    <Grid container className={classes.todoWrapper}>
+      <Grid item className={classes.todoHeader}>
+        <h3>What Needs to Get Done?</h3>
+        <form>
+          <Input
+            className={classes.input}
+            placeholder='Feed the butterflies'
+            autoFocus='true'
+            onChange={handleChange()}
+          />
+          <Button
+            variant='outlined'
+            style={{ marginLeft: '30px' }}
+          >
+            Add
+          </Button>
+        </form>
+      </Grid>
+      <Grid
+        container
+        className={classes.columnWrapper}
+      >
+        <div className={classes.finished}>
+          <h2>To Do</h2>
+          {todos.map((item) => (
+            <div key={item.id}>
+              <ul className={classes.listItems}>
+                <li>{item.name}</li>
+                <Button onClick={completeItem(item)}
+                  className={classes.button}>Complete</Button>
+              </ul>
+            </div>
+          ))}
+          <div className={classes.clearButton}>
+            <Button onClick={clearItems()}
+              className={classes.button}>Clear List</Button>
+          </div>
+        </div>
+        <div className={classes.finished}>
+          <h2>Finished</h2>
+          {finished.map((item) => (
+            <ul key={item.id}
+              className={classes.listItems}>
+              <li>{item.name}</li>
+              <Button onClick={deleteItem(item)}
+                className={classes.button}>x</Button>
+            </ul>
+          ))}
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
---
+++ src/components/data.js
@@ -0,0 +1,38 @@
+// import faker from "faker";
+
+// export const fakerData = new Array(20).fill().map(() => {
+//   return {
+//     id: faker.random.uuid(),
+//     name: `${faker.name.firstName()} ${faker.name.lastName()}`
+//   };
+// });
+
+export const customData = [
+  {
+    name: 'Wash the dishes',
+    id: 1,
+  },
+  {
+    name: 'Take out the trash',
+    id: 2,
+
+  },
+  {
+    name: 'Do the laundry',
+    id: 3,
+
+  },
+  {
+    name: 'Brush teeth',
+    id: 4,
+
+  },
+  {
+    name: 'Take a shower',
+    id: 5,
+  },
+  {
+    name: 'Do a barrel roll',
+    id: 6,
+  }
+]
